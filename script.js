2b4726a975624adf315fcbfab825ce31d4747eac
RizkyApiip <63902203+RizkyApiip@users.noreply.github.com>
2023-05-01 19:27:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// tombol class active
+const navbar = document.querySelector('nav .navbar');
+// ketika hamburger menu di pencet
+document.querySelector('#navbar_menu').
+onclick = ( ) => {
+    navbar.classList.toggle('active');
+};
